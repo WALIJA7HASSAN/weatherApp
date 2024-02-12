60f7342165784e6ecf323422c13d03eb4aab2813
WALIJA7HASSAN <syedawalijahassan@gmail.com>
2024-02-12 17:46:35 +0500

    adding loc btn

--- app.js
+++ app.js
@@ -24,30 +24,9 @@ const city=getElement('.city');
 const weatherdesc=getElement('.weather-desc');
 const wind=getElement('.wind');
 const DomHumidity=getElement('.humidity');
-const card=getElement('.card');
+// const card=getElement('.card');
+const LocBtn=getElement('.loc-btn');
 
-const geoApi="da6fe7bffc4742fa973debda203f00b8";
-// using api to get city location
-if("geolocation" in navigator){
-    navigator.geolocation.getCurrentPosition(function(position){
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-
-    // Use latitude and longitude to fetch city name from a geocoding service
-    // This example uses OpenCage Geocoding API
-    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${geoApi}`)
-        .then(response => response.json())
-        .then(data => {
-            const geocity = data.results[0].components.city;
-            console.log("City:", geocity);
-            checkWeather(geocity);
-        })
-        .catch(error => console.error("Error fetching city:", error));
-    })
-}
-else{
-    console.log("Geolocation is not supported by this browser.");
-}
 const checkWeather=async(loc)=>{
     try {
         const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${loc}&appid=baa15ac75026e8c0c7d2b8fed1275c5b`)
@@ -86,14 +65,14 @@ switch (main) {
         weatherImg.src='./images/sun(1).png';
         break;
 }
-if(main.toLowerCase().includes('cloud')){
-    card.classList.remove('bg-yellow-400');
-    card.classList.add('bg-blue-600');
-    }
-    else{
-        card.classList.add('bg-yellow-400');
-        card.classList.remove('bg-blue-600');
-    }
+// if(main.toLowerCase().includes('cloud')){
+//     card.classList.remove('bg-yellow-400');
+//     card.classList.add('bg-blue-600');
+//     }
+//     else{
+//         card.classList.add('bg-yellow-400');
+//         card.classList.remove('bg-blue-600');
+//     }
     } catch (error) {
         console.error("Error fetching data",error);
     }
@@ -102,4 +81,29 @@ if(main.toLowerCase().includes('cloud')){
 
 searchBtn.addEventListener('click',()=>{
     checkWeather(searchInput.value);
+})
+
+LocBtn.addEventListener('click',()=>{
+    const geoApi="da6fe7bffc4742fa973debda203f00b8";
+// using api to get city location
+if("geolocation" in navigator){
+    navigator.geolocation.getCurrentPosition(function(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    // Use latitude and longitude to fetch city name from a geocoding service
+    // This example uses OpenCage Geocoding API
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${geoApi}`)
+        .then(response => response.json())
+        .then(data => {
+            const geocity = data.results[0].components.city;
+            console.log("City:", geocity);
+            checkWeather(geocity);
+        })
+        .catch(error => console.error("Error fetching city:", error));
+    })
+}
+else{
+    console.log("Geolocation is not supported by this browser.");
+}
 })
